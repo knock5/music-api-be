e77ad5d0e61827579f5fdf3d414ef6b38b58817d
knock5 <miftachulhd5@gmail.com>
2023-07-29 20:43:52 +0700

    create table migration

---
+++ migrations/1690637477654_create-table-songs.js
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+      nullable: true,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      references: '"albums"',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    inserted_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
