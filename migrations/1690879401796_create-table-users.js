8f78b2543a942414813ebb57c81f8791e265517d
knock5 <miftachulhd5@gmail.com>
2023-08-01 15:47:27 +0700

    Create 1690879401796_create-table-users.js

---
+++ migrations/1690879401796_create-table-users.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
