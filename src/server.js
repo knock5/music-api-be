8a29970807e5f640423767853964bb8c45c44f5d
knock5 <miftachulhd5@gmail.com>
2023-08-01 22:17:00 +0700

    add: create handler users

--- src/server.js
+++ src/server.js
@@ -3,16 +3,20 @@ const Hapi = require('@hapi/hapi');
 // plugins
 const albums = require('./api/albums');
 const songs = require('./api/songs');
+const users = require('./api/users');
 // services
 const AlbumsService = require('./services/AlbumServices');
 const SongsService = require('./services/SongService');
+const UserService = require('./services/UserServices');
 // validator
 const AlbumValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
+const UserValidator = require('./validator/users');
 
 const init = async () => {
   const songsService = new SongsService();
   const albumsService = new AlbumsService();
+  const userService = new UserService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -39,6 +43,16 @@ const init = async () => {
       validator: AlbumValidator,
     },
   });
+
+  await server.register([
+    {
+      plugin: users,
+      options: {
+        service: userService,
+        validator: UserValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
