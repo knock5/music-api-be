37a0a84d4ea3c89db08725739ba6bfaa44fdb18a
knock5 <miftachulhd5@gmail.com>
2023-08-01 22:02:19 +0700

    add: create plugins

---
+++ src/api/users/handler.js
@@ -0,0 +1,81 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    const postUserHandler = this.postUserHandler.bind(this);
+    const getUserByIdHandler = this.getUserByIdHandler.bind(this);
+  }
+
+  async postUserHandler(request, h) {
+    try {
+      this._validator.validateUserPayload(request.payload);
+      const { username, password, fullname } = request.payload;
+
+      const userId = await this._service.addUser({ username, password, fullname });
+
+      const response = h.response({
+        status: 'success',
+        message: 'user berhasil ditambahkan',
+        data: {
+          userId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          Message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+      // Server Error
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kesalahan pada server kami...',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async getUserByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const user = await this._service.getUserById(id);
+      const response = h.response({
+        status: 'success',
+        data: {
+          user,
+        },
+      });
+      response.code(200);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          Message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+      // Server Error
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kesalahan pada server kami...',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = UsersHandler;
