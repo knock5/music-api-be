98cb32348e7723d4240567e5bb5a1d9dcec681aa
knock5 <miftachulhd5@gmail.com>
2023-08-18 17:27:55 +0700

    add: validation playlist and create table collaborations

---
+++ src/validator/playlists/index.js
@@ -0,0 +1,31 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const {
+  PostPlaylistPayloadSchema,
+  PostSongPayloadSchema,
+  PostActivityPayloadSchema,
+} = require('./schema');
+
+const PlaylistsValidator = {
+  validatePlaylistPayload: (payload) => {
+    const resValidation = PostPlaylistPayloadSchema.validate(payload);
+    if (resValidation.error) {
+      throw new InvariantError(resValidation.error.message);
+    }
+  },
+
+  validatePostSongPayload: (payload) => {
+    const resValidation = PostSongPayloadSchema.validate(payload);
+    if (resValidation.error) {
+      throw new InvariantError(resValidation.error.message);
+    }
+  },
+
+  validateActivityPayload: (payload) => {
+    const resValidation = PostActivityPayloadSchema.validate(payload);
+    if (resValidation.error) {
+      throw new InvariantError(resValidation.error.message);
+    }
+  },
+};
+
+module.exports = PlaylistsValidator;
